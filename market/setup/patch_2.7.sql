66c3144c7e397d21c99f7026c77f2d61a806a25f
webitproff <webitproff@gmail.com>
2023-02-03 05:55:00 +0200

    Delete patch_2.7.sql

--- market/setup/patch_2.7.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `cot_market_types` (
-  `type_id` int(10) unsigned NOT NULL auto_increment,
-  `type_title` varchar(255) collate utf8_unicode_ci DEFAULT '',
-  PRIMARY KEY  (`type_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
